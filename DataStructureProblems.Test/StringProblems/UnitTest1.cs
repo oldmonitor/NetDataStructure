b67ac391cee4945fce0f3a9d7fa523ae4a51128d
Derek Ho <derek.ho@prutech.com>
2020-01-18 18:06:36 -0500

    longest common prefix

---
+++ DataStructureProblems.Test/StringProblems/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataStructureProblems.StringProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.StringProblems
+{
+    [TestClass]
+    public class LongestCommonPrefixTest
+    {
+        [TestMethod]
+        public void TestGetLongestCommonPrefix()
+        {
+            LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+            string[] stringArray = new string[] { "abc", "ab" };
+            string prefix = longestCommonPrefix.GetLongestCommonPrefix(stringArray);
+
+            Assert.AreEqual("ab", prefix);
+        }
+
+        [TestMethod]
+        public void TestEmptyPrefix()
+        {
+            LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+            string[] stringArray = new string[] { "abc", "efg" };
+            string prefix = longestCommonPrefix.GetLongestCommonPrefix(stringArray);
+
+            Assert.AreEqual("", prefix);
+        }
+    }
+}
---
+++ DataStructureProblems/StringProblems/1_longestCommonPrefix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.StringProblems
+{
+    public class LongestCommonPrefix
+    {
+        public string GetLongestCommonPrefix(string[] stringArray)
+        {
+            int minLength = 0;
+            
+            if(stringArray.Length == 0)
+            {
+                return "";
+            }
+            minLength = this.FindMinLength(stringArray);
+
+            string commonPrefix = "";
+            for(int i = 1;i<=minLength;i++)
+            {
+                string substring = stringArray[0].Substring(0, i);
+                bool isCommonPrefix = true;
+                for(int j = 1; j< stringArray.Length; j++)
+                {
+                    if(stringArray[j].StartsWith(substring) == false)
+                    {
+                        isCommonPrefix = false;
+                        break;
+                    }
+                }
+
+                if(isCommonPrefix == false)
+                {
+                    break;
+                }
+                else
+                {
+                    commonPrefix = substring;
+                }
+            }
+            return commonPrefix;
+        }
+
+        private int FindMinLength(string[] stringArray)
+        {
+            if (stringArray.Length == 0)
+            {
+                return 0;
+            }
+
+            int minLength = stringArray[0].Length;
+            for(int i= 1;i<stringArray.Length;i++)
+            {
+                if(stringArray[i].Length < minLength)
+                {
+                    minLength = stringArray[i].Length;
+                }
+            }
+            return minLength;
+        }
+    }
+}
