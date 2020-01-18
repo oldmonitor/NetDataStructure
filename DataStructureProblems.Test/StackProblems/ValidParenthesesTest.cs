5358a2ef3601c1c03af126cecdcbd987e00872aa
Derek Ho <derek.ho@prutech.com>
2020-01-18 18:46:09 -0500

    check invalid parentheses

---
+++ DataStructureProblems.Test/StackProblems/ValidParenthesesTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DataStructureProblems.StackProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.StackProblems
+{
+    [TestClass]
+    public class ValidParenthesesTest
+    {
+        [TestMethod]
+        public void TestValidParen()
+        {
+            ValidParentheses validParentheses = new ValidParentheses();
+            bool isValid = validParentheses.IsParenthesesValid("{}[](test)");
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void TestValidParenEmptyString()
+        {
+            ValidParentheses validParentheses = new ValidParentheses();
+            bool isValid = validParentheses.IsParenthesesValid("");
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void TestInValidParenString()
+        {
+            ValidParentheses validParentheses = new ValidParentheses();
+            bool isValid = validParentheses.IsParenthesesValid("{");
+            Assert.IsFalse(isValid);
+
+            isValid = validParentheses.IsParenthesesValid("{}]");
+            Assert.IsFalse(isValid);
+        }
+
+
+
+
+    }
+}
--- DataStructureProblems/StackProblems/1_ValidParentheses.cs
+++ DataStructureProblems/StackProblems/1_ValidParentheses.cs
@@ -8,6 +8,57 @@ namespace DataStructureProblems.StackProblems
 {
     public class ValidParentheses
     {
+        private Dictionary<char, char> _openParen;
+        private Dictionary<char, char> _closeParen;
 
+        private void InitParentheseHashtable()
+        {
+            _openParen = new Dictionary<char, char>();
+            _openParen.Add('(', ')');
+            _openParen.Add('[', ']');
+            _openParen.Add('{', '}');
+
+            _closeParen = new Dictionary<char, char>();
+            _closeParen.Add(')', '(');
+            _closeParen.Add(']', '[');
+            _closeParen.Add('}', '{');
+        }
+
+        public bool IsParenthesesValid(string str)
+        {
+            Stack<char> parenStack = new Stack<char>();
+            this.InitParentheseHashtable();
+            if(str.Length == 0)
+            {
+                return true;
+            }
+            foreach(char c in str)
+            {
+                // if c is open paren, push to stack
+                if(_openParen.ContainsKey(c))
+                {
+                    parenStack.Push(c);
+                }
+
+                // if c is close paren, check the top of stack
+                if(_closeParen.ContainsKey(c))
+                {
+                    if(parenStack.Count == 0)
+                    {
+                        return false;
+                    }
+                    if(parenStack.Pop() != _closeParen[c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if(parenStack.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
