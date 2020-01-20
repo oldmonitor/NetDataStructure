5540f8bb3849a7ba660475bfe8d007ffd4e208e1
DerekHo <DerekHo@DESKTOP-D2N9FG9>
2020-01-20 16:52:05 -0500

    add one to int data array

---
+++ DataStructureProblems.Test/ArrayProblems/AddOneToIntegerInArrayTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DataStructureProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.ArrayProblems
+{
+    [TestClass]
+    public class AddOneToIntegerInArrayTest
+    {
+        [TestMethod]
+        public void TestAddOneWithNoCarry()
+        {
+            int[] dataArray = new int[] { 1, 2 };
+            AddOneToIntegerInArray a = new AddOneToIntegerInArray();
+            int[] dataResult = a.HandleIntegerArrayPlusOne(dataArray);
+
+            Assert.AreEqual(2, dataResult.Length);
+            Assert.AreEqual(1, dataResult[0]);
+            Assert.AreEqual(3, dataResult[1]);
+        }
+
+        [TestMethod]
+        public void TestAddOneWithCarry()
+        {
+            int[] dataArray = new int[] { 3, 9, 9 };
+            AddOneToIntegerInArray a = new AddOneToIntegerInArray();
+            int[] dataResult = a.HandleIntegerArrayPlusOne(dataArray);
+
+            Assert.AreEqual(3, dataResult.Length);
+            Assert.AreEqual(4, dataResult[0]);
+            Assert.AreEqual(0, dataResult[1]);
+            Assert.AreEqual(0, dataResult[2]);
+
+        }
+
+        [TestMethod]
+        public void TestAddOneWithCarryNeedExtraDigit()
+        {
+            int[] dataArray = new int[] { 9, 9, 9 };
+            AddOneToIntegerInArray a = new AddOneToIntegerInArray();
+            int[] dataResult = a.HandleIntegerArrayPlusOne(dataArray);
+
+            Assert.AreEqual(4, dataResult.Length);
+            Assert.AreEqual(1, dataResult[0]);
+            Assert.AreEqual(0, dataResult[1]);
+            Assert.AreEqual(0, dataResult[2]);
+            Assert.AreEqual(0, dataResult[3]);
+
+        }
+
+    }
+}
---
+++ DataStructureProblems/ArrayProblems/7_AddOneToIntegerInArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.ArrayProblems
+{
+    public class AddOneToIntegerInArray
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataArray"></param>
+        /// <returns></returns>
+        public int[] HandleIntegerArrayPlusOne(int[] dataArray)
+        {
+            if(dataArray== null || dataArray.Length == 0)
+            {
+                return dataArray;
+            }
+
+            // start from the last item, add one if there is carry, check the next digit
+            int index = dataArray.Length - 1;
+            bool isCarry = false;
+            for(int i = index; i>=0; i--)
+            {
+                dataArray[i] = dataArray[i] + 1;
+                if(dataArray[i] < 10)
+                {
+                    isCarry = false;
+                    break;
+                }
+                else
+                {
+                    dataArray[i] = dataArray[i] % 10;
+                    isCarry = true;
+                }
+            }
+
+            if(isCarry == true)
+            {
+                int[] dataArrayResult = new int[dataArray.Length + 1];
+                dataArrayResult[0] = 1;
+                for(int i=0;i<dataArray.Length; i++)
+                {
+                    dataArrayResult[i + 1] = dataArray[i];
+                }
+                return dataArrayResult;
+            }
+            else
+            {
+                return dataArray;
+            }
+        }
+    }
+}
