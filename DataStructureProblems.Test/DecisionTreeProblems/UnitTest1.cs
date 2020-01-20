81e9a00d0ae3ecd899176980012885f039ebb83b
DerekHo <DerekHo@DESKTOP-D2N9FG9>
2020-01-20 12:42:54 -0500

    climb stair decision tree problem

---
+++ DataStructureProblems.Test/DecisionTreeProblems/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DataStructureProblems.DecisionTreeProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.DecisionTreeProblems
+{
+    [TestClass]
+    public class ClimbStairsTest
+    {
+        [TestMethod]
+        public void TestWithOnlyOneStep()
+        {
+            ClimbStairs c = new ClimbStairs();
+            int numberOfWay = c.FindNumberOfWayOfClimbingStairs(1);
+            Assert.AreEqual(1, numberOfWay);
+        }
+
+        [TestMethod]
+        public void TestWithOnlyTwoStep()
+        {
+            ClimbStairs c = new ClimbStairs();
+            int numberOfWay = c.FindNumberOfWayOfClimbingStairs(2);
+            Assert.AreEqual(2, numberOfWay);
+        }
+
+        [TestMethod]
+        public void TestWithOnlyThreeStep()
+        {
+            ClimbStairs c = new ClimbStairs();
+            int numberOfWay = c.FindNumberOfWayOfClimbingStairs(3);
+            Assert.AreEqual(3, numberOfWay);
+        }
+
+    }
+}
---
+++ DataStructureProblems/DecisionTreeProblems/1_ClimbStairs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.DecisionTreeProblems
+{
+    public class ClimbStairs
+    {
+        /*
+            You are climbing a stair case. It takes n steps to reach to the top.
+            Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+        */
+
+        /// <summary>
+        /// solutin is finding number of path that can goes down the decision tree
+        /// </summary>
+        /// <param name="numberOfStep"></param>
+        /// <returns></returns>
+        public int FindNumberOfWayOfClimbingStairs(int numberOfStep)
+        {
+            return GetNumberStep(numberOfStep);
+        }
+
+        private int GetNumberStep(int stepRemaining)
+        {
+            if(stepRemaining == 0)
+            {
+                return 1;
+            }
+
+            // can't take more steps than stepRemaining
+            if(stepRemaining < 0)
+            {
+                return 0;
+            }
+
+            // subtree of taking one step plus subtree of taking two steps
+            return GetNumberStep(stepRemaining - 1) + GetNumberStep(stepRemaining - 2);
+        }
+    }
+}
