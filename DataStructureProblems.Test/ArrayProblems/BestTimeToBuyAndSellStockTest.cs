4773dde1a18c4f80a16ceebd8d07599c91f4e446
Derek Ho <derek.ho@prutech.com>
2020-01-20 21:31:31 -0500

    problem: best time to buy and sell stock

---
+++ DataStructureProblems.Test/ArrayProblems/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,41 @@
+﻿using DataStructureProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.Test.ArrayProblems
+{
+    [TestClass]
+    public class BestTimeToBuyAndSellStockTest
+    {
+        [TestMethod]
+        public void TestArrayWithJustOneItem()
+        {
+            int[] dataArray = new int[] { 1 };
+            BestTimeToBuyAndSellStock c = new BestTimeToBuyAndSellStock();
+            int profit = c.GetMaxProfit(dataArray);
+            Assert.AreEqual(0, profit);
+        }
+
+        [TestMethod]
+        public void TestArrayWithTwoItem()
+        {
+            int[] dataArray = new int[] { 1, 4 };
+            BestTimeToBuyAndSellStock c = new BestTimeToBuyAndSellStock();
+            int profit = c.GetMaxProfit(dataArray);
+            Assert.AreEqual(3, profit);
+        }
+
+        [TestMethod]
+        public void TestArray()
+        {
+            int[] dataArray = new int[] { 7, 1, 5, 3, 6, 4 };
+            BestTimeToBuyAndSellStock c = new BestTimeToBuyAndSellStock();
+            int profit = c.GetMaxProfit(dataArray);
+            Assert.AreEqual(5, profit);
+        }
+    }
+}
---
+++ DataStructureProblems/ArrayProblems/8_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.ArrayProblems
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int GetMaxProfit(int[] stockPrice)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+            for(int i=0;i<stockPrice.Length;i++)
+            {
+                if(stockPrice[i] < minPrice)
+                {
+                    minPrice = stockPrice[i];
+                }
+                else
+                {
+                    if((stockPrice[i] - minPrice) > maxProfit)
+                    {
+                        maxProfit = stockPrice[i] - minPrice;
+                    }
+                }
+            }
+
+            return maxProfit;
+
+        }
+    }
+}
