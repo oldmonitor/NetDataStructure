61f32916ce61f03bb3a051b4b84f5e2532608359
Derek Ho <derek.ho@prutech.com>
2020-01-22 23:02:12 -0500

    Added test case for subsets

---
+++ DataStructureProblems.Test/BacktrackingProblems/FindAllPossibleSubsetsTest.cs
@@ -0,0 +1,32 @@
+﻿using DataStructureProblems.BacktrackingProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.Test.BacktrackingProblems
+{
+    [TestClass]
+    public class FindAllPossibleSubsetsTest
+    {
+        [TestMethod]
+        public void TestListWithJustOneItem()
+        {
+            FindAllPossibleSubsets c = new FindAllPossibleSubsets();
+            List<List<string>> result = c.HandleFindAllPossibleSubsets(new List<string>() { "a" });
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("a", result[0][0]);
+        }
+
+        [TestMethod]
+        public void TestListWithJustTwoItem()
+        {
+            FindAllPossibleSubsets c = new FindAllPossibleSubsets();
+            List<List<string>> result = c.HandleFindAllPossibleSubsets(new List<string>() { "a", "b", "c" });
+            Assert.AreEqual(7, result.Count);
+        }
+
+    }
+}
---
+++ DataStructureProblems/BacktrackingProblems/2_FindAllPossibleSubsets.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.BacktrackingProblems
+{
+    public class FindAllPossibleSubsets
+    {
+        public List<List<string>> HandleFindAllPossibleSubsets(List<string> input)
+        {
+            List<List<string>> result = new List<List<string>>();
+            List<string> subset = new List<string>();
+            recursiveFindPossibleSubsets(result, input, 0, subset);
+            return result;
+        }
+
+        /// <summary>
+        /// think of this problem as tree traval. each path is one possible subset. Check the graph
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="input"></param>
+        /// <param name="position"></param>
+        /// <param name="subset"></param>
+        private void recursiveFindPossibleSubsets(List<List<string>> result, List<string> input, int position, List<string> subset)
+        {
+            if(position == input.Count)
+            {
+                if(subset.Count > 0)
+                    result.Add(new List<string>(subset));
+                return;
+            }
+
+            // go the substree excluding the current character
+            recursiveFindPossibleSubsets(result, input, position + 1, subset);
+
+            // add the current character the subset
+            subset.Add(input[position]);
+
+            // go to the subtree including the current character
+            recursiveFindPossibleSubsets(result, input, position + 1, subset);
+
+            //reset
+            subset.RemoveAt(subset.Count - 1);
+        }
+
+    }
+    
+}
