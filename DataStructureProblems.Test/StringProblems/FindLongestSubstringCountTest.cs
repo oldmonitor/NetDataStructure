b3f9be127ea58f40cb648ab7ec962b2afdc5c15f
Derek Ho <derek.ho@prutech.com>
2020-01-25 14:15:21 -0500

    longest substring

---
+++ DataStructureProblems.Test/StringProblems/FindLongestSubstringCountTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DataStructureProblems.StringProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.StringProblems
+{
+    [TestClass]
+    public class FindLongestSubstringCountTest
+    {
+        [TestMethod]
+        public void TestWithJustOneCharacter()
+        {
+            FindLongestSubstringCount c = new FindLongestSubstringCount();
+            string s = "a";
+            int len = c.HandleFindLongestSubstringCount(s);
+            Assert.AreEqual(1, len);
+        }
+
+        [TestMethod]
+        public void TestWithLongestSubstringAtTheEnd()
+        {
+            FindLongestSubstringCount c = new FindLongestSubstringCount();
+            string s = "ababcabcd";
+            int len = c.HandleFindLongestSubstringCount(s);
+            Assert.AreEqual(4, len);
+        }
+
+        [TestMethod]
+        public void TestWithLongestSubstringInMiddle()
+        {
+            FindLongestSubstringCount c = new FindLongestSubstringCount();
+            string s = "ababcdeabcd";
+            int len = c.HandleFindLongestSubstringCount(s);
+            Assert.AreEqual(5, len);
+        }
+    }
+}
---
+++ DataStructureProblems/StringProblems/3_FindLongestSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.StringProblems
+{
+    public class FindLongestSubstringCount
+    {
+        /// <summary>
+        /// return the longest substring with no repeating character. 
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public int HandleFindLongestSubstringCount(string str)
+        {
+            int i = 0; // beginning of sliding window
+            int j = 0; // ending index of sliding window
+            int[] counter = new int[128]; //counter for character in the sliding window
+            int maxLength = 0;
+            for (j=0;j<str.Length; j++)
+            {
+                //increase count
+                counter[str[j]]++;
+                while(counter[str[j]] > 1)
+                {
+                    counter[str[i]]--;
+                    i++;
+                }
+                maxLength = Math.Max(maxLength, j-i+1);
+            }
+
+            return maxLength;
+        }
+    }
+}
