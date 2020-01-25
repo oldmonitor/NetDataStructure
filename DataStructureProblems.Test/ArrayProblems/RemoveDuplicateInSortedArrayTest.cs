5545db224de8fec28b351113ff43a38bfe3a4032
Derek Ho <derek.ho@prutech.com>
2020-01-25 17:13:08 -0500

    remove duplicate in sorted array

---
+++ DataStructureProblems.Test/ArrayProblems/RemoveDuplicateInSortedArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataStructureProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.ArrayProblems
+{
+    [TestClass]
+    public class RemoveDuplicateInSortedArrayTest
+    {
+        [TestMethod]
+        public void TestOneDuplicateIntArray()
+        {
+            int[] data = new int[] { 5, 5 };
+            RemoveDuplicateInSortedArray c = new RemoveDuplicateInSortedArray();
+            int[] result = c.HandleRemoveDuplicateInSortedArray(data);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(5, result[0]);
+        }
+
+        [TestMethod]
+        public void TestMultipleDuplicateIntArray()
+        {
+            int[] data = new int[] { 5, 5 , 7, 7, 7, 10};
+            RemoveDuplicateInSortedArray c = new RemoveDuplicateInSortedArray();
+            int[] result = c.HandleRemoveDuplicateInSortedArray(data);
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual(5, result[0]);
+            Assert.AreEqual(7, result[1]);
+            Assert.AreEqual(10, result[2]);
+
+        }
+    }
+}
---
+++ DataStructureProblems/ArrayProblems/9_RemoveDuplicateInSortedArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.ArrayProblems
+{
+    public class RemoveDuplicateInSortedArray
+    {
+        public int[] HandleRemoveDuplicateInSortedArray(int[] input)
+        {
+            if(input == null || input.Length <= 1)
+            {
+                return input;
+            }
+
+            int i = 0; // only move when there is distinct character
+            int j;
+            for (j = 1; j< input.Length; j++)
+            {
+                // if new encounter character is different from the last distinct character,
+                // set the last distinct character and move i
+                if(input[i] != input[j])
+                {
+                    i++;
+                    input[i] = input[j];
+                }
+            }
+
+            // cut out anything after distinct character count
+            Array.Resize(ref input, i + 1);
+            return input;
+        }
+    }
+}
