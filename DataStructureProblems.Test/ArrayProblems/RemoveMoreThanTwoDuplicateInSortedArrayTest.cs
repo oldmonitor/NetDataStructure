810154ed049eef10aa682fe3b930e3668e923c36
Derek Ho <derek.ho@prutech.com>
2020-01-26 16:08:05 -0500

    remove duplicate more than once

---
+++ DataStructureProblems.Test/ArrayProblems/RemoveMoreThanTwoDuplicateInSortedArrayTest.cs
@@ -0,0 +1,75 @@
+﻿using DataStructureProblems.ArrayProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.Test.ArrayProblems
+{
+    [TestClass]
+    public class RemoveMoreThanTwoDuplicateInSortedArrayTest
+    {
+        [TestMethod]
+        public void TestDupOfOneNumber()
+        {
+            int[] input = new int[] { 1, 1, 1, 1 };
+            RemoveMoreThanTwoDuplicateInSortedArray c = new RemoveMoreThanTwoDuplicateInSortedArray();
+            int[] output = c.HandleRemoveMoreThanTwoDuplicateInSortedArray(input);
+            Assert.AreEqual(2, output.Length);
+            Assert.AreEqual(1, output[0]);
+            Assert.AreEqual(1, output[1]);
+        }
+
+        [TestMethod]
+        public void TestNoDup()
+        {
+            int[] input = new int[] { 1, 2, 3, 4 };
+            RemoveMoreThanTwoDuplicateInSortedArray c = new RemoveMoreThanTwoDuplicateInSortedArray();
+            int[] output = c.HandleRemoveMoreThanTwoDuplicateInSortedArray(input);
+            Assert.AreEqual(4, output.Length);
+            Assert.AreEqual(1, output[0]);
+            Assert.AreEqual(2, output[1]);
+            Assert.AreEqual(3, output[2]);
+            Assert.AreEqual(4, output[3]);
+
+        }
+
+
+        [TestMethod]
+        public void TestOneCharacter()
+        {
+            int[] input = new int[] { 1};
+            RemoveMoreThanTwoDuplicateInSortedArray c = new RemoveMoreThanTwoDuplicateInSortedArray();
+            int[] output = c.HandleRemoveMoreThanTwoDuplicateInSortedArray(input);
+            Assert.AreEqual(1, output.Length);
+            Assert.AreEqual(1, output[0]);
+            
+        }
+
+        [TestMethod]
+        public void TestDupOfMultipleNumber()
+        {
+            int[] input = new int[] { 1, 1, 1, 1, 2, 3, 3, 3, 4, 4 };
+            RemoveMoreThanTwoDuplicateInSortedArray c = new RemoveMoreThanTwoDuplicateInSortedArray();
+            int[] output = c.HandleRemoveMoreThanTwoDuplicateInSortedArray(input);
+            Assert.AreEqual(7, output.Length);
+            Assert.AreEqual(1, output[0]);
+            Assert.AreEqual(1, output[1]);
+
+            Assert.AreEqual(2, output[2]);
+
+            Assert.AreEqual(3, output[3]);
+
+            Assert.AreEqual(3, output[4]);
+            Assert.AreEqual(4, output[5]);
+            Assert.AreEqual(4, output[6]);
+        }
+
+
+
+
+
+    }
+}
---
+++ DataStructureProblems/ArrayProblems/11_RemoveMoreThanTwoDuplicateInSortedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.ArrayProblems
+{
+    /// <summary>
+    /// remove characters that are duplicated more than 2 times.
+    ///  111223 => 11223
+    /// </summary>
+    public class RemoveMoreThanTwoDuplicateInSortedArray
+    {
+        public int[] HandleRemoveMoreThanTwoDuplicateInSortedArray(int[] input)
+        {
+            // if length is less than 2, just return
+            if(input == null || input.Length <= 2)
+            {
+                return input;
+            }
+
+            int i = 2;
+            int j;
+
+            for(j=2; j<input.Length; j++)
+            {
+                if(input[j] != input[i-2]) //max 2 duplicate, third characher should be different
+                {
+                    input[i] = input[j];
+                    i++;
+                }
+            }
+
+            Array.Resize(ref input, i);
+            return input;
+        }
+
+
+    }
+}
