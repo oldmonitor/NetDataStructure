f0f32f8f70cf362b72aab466b8e4f95103d66d48
Derek Ho <derek.ho@prutech.com>
2020-01-26 17:32:44 -0500

    find first unique character in string

---
+++ DataStructureProblems.Test/HashTableProblems/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataStructureProblems.HashtableProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureProblems.Test.HashTableProblems
+{
+    [TestClass]
+    public class FindFirstUniqueCharacterInStringTest
+    {
+        [TestMethod]
+        public void TestOneCharacterArray()
+        {
+            string input = "a";
+            FindFirstUniqueCharacterInString c = new FindFirstUniqueCharacterInString();
+            var output = c.GetFirstUniqueCharacterInString(input);
+            Assert.AreEqual('a', output);
+        }
+
+        [TestMethod]
+        public void TestUniqueCharacterAtTheEnd()
+        {
+            string input = "aaababc";
+            FindFirstUniqueCharacterInString c = new FindFirstUniqueCharacterInString();
+            var output = c.GetFirstUniqueCharacterInString(input);
+            Assert.AreEqual('c', output);
+        }
+
+
+    }
+}
---
+++ DataStructureProblems/HashtableProblems/1_FindFirstUniqueCharacterInString.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.HashtableProblems
+{
+    public class FindFirstUniqueCharacterInString
+    {
+        public char? GetFirstUniqueCharacterInString(string input)
+        {
+            if(input == null || input.Length == 0)
+            {
+                return null;
+            }
+
+            var characterCounter = new Dictionary<char, int>();
+            var characterPosition = new Dictionary<char, int>();
+
+            for(int i = 0;i<input.Length; i++)
+            {
+                char c = input[i];
+                if(characterCounter.ContainsKey(c) == false)
+                {
+                    characterCounter.Add(c, 1);
+                }
+                else
+                {
+                    characterCounter[c]++;
+                }
+
+                if (characterPosition.ContainsKey(c) == false)
+                {
+                    characterPosition.Add(c, 1);
+                }
+            }
+
+            char? firstUniqueCharacter = null;
+            int index = int.MaxValue;
+            foreach(char c in characterCounter.Keys)
+            {
+                if(characterCounter[c] == 1)
+                {
+                    if(characterPosition[c] < index)
+                    {
+                        index = characterPosition[c];
+                        firstUniqueCharacter = c;
+                    }
+                }
+            }
+
+            return firstUniqueCharacter;
+        }
+    }
+}
